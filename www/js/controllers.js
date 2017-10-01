f6e97a8e258640959d77ebb75c9fe1a5a0b236e8
Mateo Sixtos <msixtos@csumb.edu>
2017-10-01 14:03:36 -0700

    better

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,20 +8,15 @@ angular.module('starter.controllers', [])
 .controller('FriendsCtrl', function($scope) {
   // $scope.friends = Friends.all();
 })
+
+
 .controller('AccountCtrl', function($scope) {
-  });
+  })
   
 
 // .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
 //   $scope.friend = Friends.get($stateParams.friendId);
 // })
-
-<<<<<<< HEAD
-.controller('AccountCtrl', function($scope) {
-})
-
-=======
->>>>>>> 2c6fac1ef40444cafa2aab5461194ac37b0cf59d
 
 
 
