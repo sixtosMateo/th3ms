1975bab0ec63b95882524ce143080864a718233b
Mateo Sixtos <msixtos@csumb.edu>
2017-10-01 01:45:48 -0700

    change friends tab

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,13 +3,14 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+// erase ,Friends inside function
+.controller('FriendsCtrl', function($scope) {
+  // $scope.friends = Friends.all();
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+// .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+//   $scope.friend = Friends.get($stateParams.friendId);
+// })
 
 .controller('AccountCtrl', function($scope) {
 });
